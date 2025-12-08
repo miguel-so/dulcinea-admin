8819043d05332bddece6e5ec028da4192d512d35
miguel-so <software.adhoc.87@gmail.com>
2025-12-08 10:13:24 -0500

    limit api call issue fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,8 +35,8 @@ app.use(helmet());
 
 // Rate limiting
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 60 * 1000, // 1 minute
+  max: 1000, // limit each IP to 1000 requests per windowMs
   message: "Too many requests from this IP, please try again later.",
 });
 app.use(limiter);
